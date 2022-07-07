b58140ae25db8597523474ac0425130fe61a5c61
Hyeonkkyu <101560173+Hyeonkkyu@users.noreply.github.com>
2022-07-07 16:36:10 +0900

    first commit

---
+++ js/galmegibrewing.js
@@ -0,0 +1,20 @@
+$(function () {
+    $('.mainSlider').slick({
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+    });
+
+    $('.beerSlider').slick({
+        slidesToShow: 3,
+        autoplaySpeed: 4000,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        pauseOnHover: false,
+    });
+
+
+
+})
